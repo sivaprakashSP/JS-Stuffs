ab4be74a88537f10ade2107a2a3d866c248f7af0
sivaprakashSP <sivaprakashsp@gmail.com>
2020-12-20 22:53:34 +0530

    JS Crash Course 1st part

---
+++ JS_Crash_Course/home.js
@@ -0,0 +1,19 @@
+console.log('Hello SP!!');
+//alert('This is SP!!');
+
+//How to write comment inline
+
+//Variables
+var b='smoothie';
+console.log(b);
+
+var num=45;
+console.log(num);
+
+document.getElementById('text').innerHTML="Hey Siva!!";
+//document.getElementById('Sometext').innerHTML="Hey There!!";
+
+//prompt('What is your age?');
+
+var age=prompt('What is your age?');
+document.getElementById('Sometext').innerHTML=age;
